e042e40546f2674302429c75821315744135f52c
youssof707 <youssoffahmy@gmail.com>
2021-11-07 15:19:44 -0500

    added logging to catchAllErrors

--- src/middleware/catchAllErrors.ts
+++ src/middleware/catchAllErrors.ts
@@ -1,8 +1,12 @@
 import {Request, Response, NextFunction} from 'express'
+import { printLog } from '..'
 
 type ErrorHandler = (req:Request, res:Response, next:NextFunction, err:any) => any
 
-const defaultErrorHandler = (_req:Request, res:Response) => res.status(500).json({ error: 'Something went wrong!' })
+const defaultErrorHandler = (_req:Request, res:Response, _next:NextFunction, err:any) => {
+	printLog(err)
+	return res.status(500).json({ error: 'Something went wrong!' })
+}
 
 const catchAllErrors = (onError:ErrorHandler = defaultErrorHandler) => (req:any, res: any, next:any) => {
 	try {
